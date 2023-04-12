ca6cf0aefc8a0f1968a65047d4f07df123f8e347
brunomorseg <brunomorseg@gmail.com>
2023-04-12 15:50:08 -0300

    atualização 12-04-2023

--- script.js
+++ script.js
@@ -63,3 +63,43 @@ const clicou3=()=>{
         sliders.setAttribute('style','margin-left:-200vw')
         
         }
+
+
+const menu = () => {
+    
+    const nav = document.querySelector('nav')
+    
+    if(nav.getAttribute('style') == 'display:none') {
+        nav.setAttribute('style','display:block')
+    } else {  nav.setAttribute('style','display:none')}
+    
+}
+
+/*
+
+const clicou4 = () => {
+    const tupni = document.querySelector('.input2')
+
+    //if(tupni.hasAttribute("placeholder")){
+   //     console.log('Kra, aqui tem placeholder SIM')
+   // } else {
+   //     alert ('Não tem mané!')
+   // }
+
+    //console.log(tupni.getAttribute('type'))
+    //console.log(tupni.getAttribute('placeholder'))
+    //console.log(tupni.getAttribute('class'))
+    
+    //Uso para colocar ou alterar o atributo
+
+    //tupni.setAttribute('type','Placeholder alterado')
+
+    const bot = document.querySelector('.botao')
+
+    if (tupni.getAttribute('type') === 'text' ) {
+        tupni.setAttribute('type', 'password')
+        bot.innerText = 'Mostrar Senha'
+     } else {
+        tupni.setAttribute('type','text')
+        bot.innerText = 'Ocultar Senha'
+     }}*/
